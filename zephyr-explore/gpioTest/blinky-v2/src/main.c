1b34c4991f067a3215a40365f76a7b0c56826991
rohamkaveie <roham.kaveie81@gmail.com>
2025-04-24 03:38:10 +0330

    zephyr explore : device binding
    learn how to get a device directly from devicetree or get _dt_spec of it

---
+++ zephyr-explore/gpioTest/blinky-v2/src/main.c
@@ -0,0 +1,56 @@
+/**
+ * @file 
+ * @brief blink an LED by toggeling a gpio 
+ * 
+ * 	Continuing the previous topic (get access to a component like GPIO or ...)
+ * 	Another way is using <_dt_spec>
+ * 	The word spec stands for specification and it refers to a struct that encapsulates 
+ *  all the necessary information about a device or peripheral (like device pointers and configuration)
+ *  in one place. you can use <*_DT_SPEC_GET> functions to get _dt_spec structure and First 
+ *  we need a devicetree node identifier. A lot of MACROS could make that possible such as:
+ * 	1. DT_ALIAS(alias)
+ *  2. DT_NODELABEL(label)
+ *  3. DT_CHOSEN(chosen)
+ *  4. DT_PATH(soc, pin-controller@58020000, gpio@58020000) // example path
+ * 	
+ * 
+ * @authors Roham Kaveie - Farzane Majd
+ * @date 2025-04-25
+ */
+
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+
+
+#define SLEEP_TIME_MS 500 
+
+#define LED0_NODE DT_ALIAS(led0)
+
+// if these information (NODELABEL of PORT,PIN number, and configuration flags) needed: 
+// #define LED_GPIO DT_GPIO_LABEL(LED0_NODE, gpios)
+// #define LED_PIN DT_GPIO_PIN(LED0_NODE, gpios)
+// #define LED_FLAGS DT_GPIO_FLAGS(LED0_NODE, gpios)
+
+int main(){
+	
+    struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+
+	gpio_pin_configure_dt(&led,GPIO_OUTPUT_ACTIVE);
+
+	while(1) {
+		gpio_pin_toggle_dt(&led);
+		if(gpio_pin_get_dt(&led))
+			printk("LED on \n");
+		else
+			printk("LED off \n");
+		printk("LED blinking... \n");
+		k_msleep(SLEEP_TIME_MS);
+	}
+
+}
+
+
---
+++ zephyr-explore/gpioTest/blinky/src/main.c
@@ -0,0 +1,69 @@
+	/**
+	 * @file
+	 * @brief blink an LED by toggeling a gpio 
+	 * 
+	 * 
+     * There are different ways to get access to a component like GPIO or SPI or UART or anything else. 
+	 * One way is getting the related device directly from the devicetree. 
+	 * This way is somehow uncommon and it is rarely used in the most examples which i found on Internet, mostly due to these resaons:
+	 *	1. Lack of Bundled Configuration Data
+	 *			Direct device retrieval gives you only the device pointer, without any associated configuration such as pins, flags, or other properties defined in devicetree.
+	 *			This forces you to manually fetch and manage related parameters separately, increasing boilerplate and risk of errors.
+	 *	2. Less Type Safety and Code Clarity
+	 *			Using just a device pointer can lead to scattered code where device configuration is handled separately from device access.
+	 *			dt_spec bundles device and configuration into a single, strongly typed structure (e.g., gpio_dt_spec), improving readability and reducing mistakes.
+	 *	3. More Error-Prone for Complex Bindings
+	 *			For devices that require multiple devicetree properties (e.g., GPIO pins plus flags), direct device retrieval does not help manage these cohesively.
+	 *			dt_spec macros initialize all relevant fields at compile time, preventing runtime misconfiguration.
+	 *	4. Manual Handling of Phandles and Indices	
+     *			When devices are referenced via phandles or arrays in devicetree, direct device retrieval requires manual indexing and property extraction.
+	 *			dt_spec macros abstract this complexity, making code simpler and less error-prone.
+	 *
+	 *  A device can be taken directly using <device_get_binding> function or <DEVICE_DT_GET> MACRO (using MACRO is recommended). 
+	 * 
+	 * 	Note: 
+	 * 	* There are definitely other functions or MACROs to get directly a device 
+	 *  * <device_get_binding> function looks for property of label with matched value (not node label),
+	 * 	  So you should add label property to the node in the devicetree by using an overlay. as you can see in app.overlay file.
+	 * 	*
+	 * @authors Roham Kaveie - Farzane Majd
+	 * @date 2025-04-24
+	 */
+
+
+
+
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+
+
+#define SLEEP_TIME_MS 500 
+
+
+const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(gpioe)); 
+
+int main(){
+	
+	// const struct device *dev = device_get_binding("mygpioe"); // looks for property of label with matched value (not node label)
+	// const struct device *dev = device_get_by_dt_nodelabel("gpioe");
+	// if(!dev)
+	// 	return 0;
+	gpio_pin_configure(dev, 1, GPIO_OUTPUT); // configure as output 
+	
+	while(1) {
+		gpio_pin_toggle(dev, 1); // toggles the pin
+		if(gpio_pin_get(dev,1))
+			printk("LED on \n");
+		else
+			printk("LED off \n");
+		printk("LED blinking... \n");
+		k_msleep(SLEEP_TIME_MS);
+	}
+
+}
+
+
