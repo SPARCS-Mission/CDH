ee808fcd4b7f71258feaebd020e62888be37c2f7
rohamkaveie <roham.kaveie81@gmail.com>
2025-06-26 19:47:11 +0330

    playground with Led example of fprime

---
+++ SPARCS-Mission/Components/Led/Led.hpp
@@ -0,0 +1,77 @@
+// ======================================================================
+// \title  Led.hpp
+// \author roham
+// \brief  hpp file for Led component implementation class
+// ======================================================================
+
+#ifndef Components_Led_HPP
+#define Components_Led_HPP
+
+#include "Components/Led/LedComponentAc.hpp"
+
+namespace Components {
+
+  class Led :
+    public LedComponentBase
+  {
+
+    public:
+
+      // ----------------------------------------------------------------------
+      // Component construction and destruction
+      // ----------------------------------------------------------------------
+
+      //! Construct Led object
+      Led(
+          const char* const compName //!< The component name
+      );
+
+      //! Destroy Led object
+      ~Led();
+
+
+    PRIVATE:
+    //! Emit parameter updated EVR
+    //!
+    void parameterUpdated(FwPrmIdType id  //!< The parameter ID
+                          ) override;
+
+    PRIVATE:
+
+      // ----------------------------------------------------------------------
+      // Handler implementations for typed input ports
+      // ----------------------------------------------------------------------
+
+      //! Handler implementation for run
+      //!
+      //! Port receiving calls from the rate group
+      void run_handler(
+          FwIndexType portNum, //!< The port number
+          U32 context //!< The call order
+      ) override;
+
+    PRIVATE:
+
+      // ----------------------------------------------------------------------
+      // Handler implementations for commands
+      // ----------------------------------------------------------------------
+
+      //! Handler implementation for command BLINKING_ON_OFF
+      //!
+      //! Command to turn on or off the blinking LED
+      void BLINKING_ON_OFF_cmdHandler(
+          FwOpcodeType opCode, //!< The opcode
+          U32 cmdSeq, //!< The command sequence number
+          Fw::On onOff //!< Indicates whether the blinking should be on or off
+      ) override;
+
+    Fw::On m_state = Fw::On::OFF;
+    U64 m_transitions = 0;
+    U32 m_toggleCounter = 0;
+    bool m_blinking = false;
+    
+  };
+
+}
+
+#endif
